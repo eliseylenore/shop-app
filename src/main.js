cf23c4d37904ca12108280ec3874081f46dd4786
Elise St Hilaire <eliseylenore@gmail.com>
2021-03-23 09:59:14 -0700

    add axios interceptor for 401

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 import store from "./store";
 import { BootstrapVue } from "bootstrap-vue";
 
@@ -30,6 +31,18 @@ new Vue({
       const userData = JSON.parse(userString);
       this.$store.commit("SET_USER_DATA", userData);
     }
+    axios.interceptors.response.use(
+      response => response, // simply return the response
+      error => {
+        console.log("err", error.response);
+        if (error.response.status === 401) {
+          console.log("HEY");
+          // if we catch a 401 error
+          this.$store.dispatch("logout"); // force a log out
+        }
+        return Promise.reject(error); // reject the Promise, with the error as the reason
+      }
+    );
   },
   render: h => h(App)
 }).$mount("#app");
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -5,8 +5,7 @@ const apiClient = axios.create({
   withCredentials: false, // This is the default
   headers: {
     Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: "Bearer test"
+    "Content-Type": "application/json"
   }
 });
 
@@ -17,15 +16,5 @@ export default {
   getProduct: id => apiClient.get("/products/" + id),
   registerUser: credentials => apiClient.post("/users/register/", credentials),
   loginUser: credentials => apiClient.post("/users/login/", credentials),
-  getUser: email => {
-    console.log(
-      "axios.defaults.headers ",
-      axios.defaults.headers.common["Authorization"]
-    );
-    return apiClient.get("users/" + email, {
-      headers: {
-        Authorization: axios.defaults.headers.common["Authorization"]
-      }
-    });
-  }
+  getUser: email => axios.get("http://localhost:3000/api/users/" + email)
 };
