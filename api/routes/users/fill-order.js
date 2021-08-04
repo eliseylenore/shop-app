5b576f5c7347424ef82be895b993739e183614ea
Elise St Hilaire <eliseylenore@gmail.com>
2021-08-04 11:08:35 -0700

    change fill-order to async await

--- api/routes/users/fill-order.js
+++ api/routes/users/fill-order.js
@@ -1,9 +1,9 @@
 const User = require("../../models/user");
 const { FilledOrder, PendingOrder } = require("../../models/order");
 
-module.exports = (req, res) => {
-  User.findOne({ email: req.params.email }, function(err, user) {
-    if (err) res.status(400).json(err);
+module.exports = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email }).exec();
     const { _id } = req.body;
 
     const foundOrder = user.pendingOrders.find(item => {
@@ -15,9 +15,11 @@ module.exports = (req, res) => {
       user.fulfilledOrders.push(foundOrder);
       var ObjectID = require("mongodb").ObjectID;
       const _id = new ObjectID(foundOrder._id);
-      PendingOrder.deleteOne({
-        _id
-      }).catch(err => console.log(err));
+      try {
+        await PendingOrder.deleteOne({ _id });
+      } catch (err) {
+        res.status(400).json(err);
+      }
 
       let newOrder = new FilledOrder({
         items: foundOrder.items,
@@ -26,19 +28,27 @@ module.exports = (req, res) => {
         email: foundOrder.email,
         orderDate: foundOrder.orderDate
       });
-      newOrder.save().catch(err => console.log(err));
+      try {
+        newOrder.save();
+      } catch (err) {
+        res.status(400).json(err);
+      }
       const newPendingOrders = user.pendingOrders.filter(newOrder => {
         if (newOrder !== undefined && newOrder._id !== undefined) {
           return newOrder._id.toString() !== _id.toString();
         }
       });
       user.pendingOrders = newPendingOrders;
-      user
-        .save()
-        .then(user => res.json(user))
-        .catch(err => console.log(err));
+      try {
+        await user.save();
+        res.json({ success: true, user });
+      } catch (err) {
+        res.status(400).json(err);
+      }
     } else {
       return res.status(400).json({ itemNotFound: "No item found to fulfill" });
     }
-  });
+  } catch (e) {
+    res.status(400).json(e);
+  }
 };
