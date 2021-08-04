a0952749f2d6a3a62ab7ce857ef16180d6f158f7
Elise St Hilaire <eliseylenore@gmail.com>
2021-08-04 10:43:39 -0700

    convert login-user to async await, clean up

--- api/routes/users/login-user.js
+++ api/routes/users/login-user.js
@@ -5,53 +5,46 @@ const User = require("../../models/user");
 const jwt = require("jsonwebtoken");
 const { secretOrKey } = require("../../keys");
 
-module.exports = (req, res) => {
-  // Form validation
+module.exports = async (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
   if (!isValid) {
     return res.status(400).json(errors);
   }
   const email = req.body.email;
   const password = req.body.password;
-  // Find user by email
-  User.findOne({ email }).then(user => {
-    // Check if user exists
+  try {
+    const user = await User.findOne({ email }).exec();
     if (!user) {
       return res.status(404).json({ emailnotfound: "Email not found" });
     }
     // Check password
-
-    // After editing my password, the compare function always returns false
-    // Using hash function to compare instead
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name
-        };
-        // Sign token
-        jwt.sign(
-          payload,
-          secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token,
-              email,
-              user
-            });
-          }
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
-      }
-    });
-  });
-}
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      // Create JWT Payload
+      const payload = {
+        id: user.id,
+        name: user.name
+      };
+      // Sign token
+      jwt.sign(
+        payload,
+        secretOrKey,
+        {
+          expiresIn: 31556926 // 1 year in seconds
+        },
+        (err, token) => {
+          res.json({
+            success: true,
+            token: "Bearer " + token,
+            email,
+            user
+          });
+        }
+      );
+    } else {
+      return res.status(400).json({ passwordincorrect: "Password incorrect" });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
