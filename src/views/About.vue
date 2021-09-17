b4f06b41f50c1668880d64f033d4f7d4ff9c4d3c
Elise St Hilaire <eliseylenore@gmail.com>
2021-09-17 12:17:24 -0700

    add picture and lorem ipsum to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,56 @@
   <div class="about">
     <b-container>
       <header>
-        <h1 class="mt-4">This is an about page</h1>
+        <picture>
+          <source
+            srcset="/img/about-header-large.png"
+            media="(min-width: 992px)"
+          />
+          <source
+            srcset="/img/about-header-medium.png"
+            media="(min-width: 768px)"
+          />
+          <img src="/img/about-header-small.png" alt="About us" class="w-100" />
+        </picture>
       </header>
+      <b-row>
+        <b-col md="6" offset-md="3" class="mt-5">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Habitasse platea dictumst vestibulum rhoncus est pellentesque. Magna
+            fermentum iaculis eu non diam phasellus vestibulum lorem. Enim ut
+            tellus elementum sagittis vitae et. Tincidunt augue interdum velit
+            euismod in pellentesque massa placerat duis. At augue eget arcu
+            dictum varius duis at. Habitasse platea dictumst vestibulum rhoncus
+            est pellentesque elit ullamcorper dignissim. Eu augue ut lectus
+            arcu. Fusce id velit ut tortor pretium viverra. Sit amet cursus sit
+            amet dictum. Sociis natoque penatibus et magnis dis parturient
+            montes nascetur ridiculus.
+          </p>
+          <p>
+            Est ultricies integer quis auctor elit sed vulputate mi. Est ante in
+            nibh mauris cursus mattis molestie a. Et malesuada fames ac turpis
+            egestas integer. Lacus sed turpis tincidunt id. Amet tellus cras
+            adipiscing enim eu turpis egestas. Hac habitasse platea dictumst
+            quisque sagittis purus sit. Gravida cum sociis natoque penatibus.
+            Vitae aliquet nec ullamcorper sit amet risus. Eu mi bibendum neque
+            egestas congue quisque egestas. Tempor id eu nisl nunc. Neque
+            convallis a cras semper auctor neque. Lacus sed viverra tellus in
+            hac habitasse platea dictumst. Velit dignissim sodales ut eu. Fames
+            ac turpis egestas sed tempus. Semper feugiat nibh sed pulvinar proin
+            gravida. Cursus euismod quis viverra nibh cras pulvinar. Molestie ac
+            feugiat sed lectus vestibulum mattis ullamcorper velit sed. Magna
+            eget est lorem ipsum dolor sit amet consectetur adipiscing. Lacus
+            luctus accumsan tortor posuere ac ut consequat semper. Nibh ipsum
+            consequat nisl vel. Nec nam aliquam sem et tortor. Tincidunt eget
+            nullam non nisi est. © 2015 — 2021 PRIVACY POLICY SITEMAP FONT
+            GENERATOR IMAGES PLUGINS GENERATORS SHARE THE LOREM WA SAI
+          </p>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
-<script>
-</script>
+<script></script>
