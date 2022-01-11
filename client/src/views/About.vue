e28e46ddf59bddfd67c162b5f54cd27e39d69898
Elise St Hilaire <eliseylenore@gmail.com>
2022-01-11 15:25:32 -0800

    flush out about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -14,40 +14,21 @@
           <img src="/img/about-header-small.png" alt="About us" class="w-100" />
         </picture>
       </header>
-      <div class="p-5 bg-turquoise">
-        <div>
+      <div class="p-4 bg-turquoise">
+        <div class="pr-lg-5">
+          <h2 class="text-left white">The shop-app mission</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Habitasse platea dictumst vestibulum rhoncus est pellentesque. Magna
-            fermentum iaculis eu non diam phasellus vestibulum lorem. Enim ut
-            tellus elementum sagittis vitae et. Tincidunt augue interdum velit
-            euismod in pellentesque massa placerat duis. At augue eget arcu
-            dictum varius duis at. Habitasse platea dictumst vestibulum rhoncus
-            est pellentesque elit ullamcorper dignissim. Eu augue ut lectus
-            arcu. Fusce id velit ut tortor pretium viverra. Sit amet cursus sit
-            amet dictum. Sociis natoque penatibus et magnis dis parturient
-            montes nascetur ridiculus.
-          </p>
-          <p>
-            Est ultricies integer quis auctor elit sed vulputate mi. Est ante in
-            nibh mauris cursus mattis molestie a. Et malesuada fames ac turpis
-            egestas integer. Lacus sed turpis tincidunt id. Amet tellus cras
-            adipiscing enim eu turpis egestas. Hac habitasse platea dictumst
-            quisque sagittis purus sit. Gravida cum sociis natoque penatibus.
-            Vitae aliquet nec ullamcorper sit amet risus. Eu mi bibendum neque
-            egestas congue quisque egestas. Tempor id eu nisl nunc. Neque
-            convallis a cras semper auctor neque. Lacus sed viverra tellus in
-            hac habitasse platea dictumst. Velit dignissim sodales ut eu. Fames
-            ac turpis egestas sed tempus. Semper feugiat nibh sed pulvinar proin
-            gravida. Cursus euismod quis viverra nibh cras pulvinar. Molestie ac
-            feugiat sed lectus vestibulum mattis ullamcorper velit sed. Magna
-            eget est lorem ipsum dolor sit amet consectetur adipiscing. Lacus
-            luctus accumsan tortor posuere ac ut consequat semper. Nibh ipsum
-            consequat nisl vel. Nec nam aliquam sem et tortor. Tincidunt eget
-            nullam non nisi est. © 2015 — 2021 PRIVACY POLICY SITEMAP FONT
-            GENERATOR IMAGES PLUGINS GENERATORS SHARE THE LOREM WA SAI
-          </p>
+              We strive to help our customers live out their adventures.
+            </p>
+            <p>
+              These can range from the everyday to special vacation gear, from the beach to the mountains.
+            </p>
+            <h2 class="text-left white">
+              How it started
+            </h2>
+            <p>
+              In 2020, shop-app's founder saw a need opening for quality retail gear for the everyday person. Due to the pandemic, everyone wanted to get out and enjoy the outdoors, and there was a market that merged between outdoor gear and personal apparel. We've been going strong ever since then.
+            </p>
         </div>
       </div>
     </b-container>
@@ -60,5 +41,8 @@
 .bg-turquoise {
   background-color: $turquoise-dark;
   color: white;
+}
+.bg-black {
+  background-color: black;
 }
 </style>
